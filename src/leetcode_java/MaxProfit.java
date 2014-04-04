5da81535877cd7d963a5d33db65632f8986b3034
solohsu <hsushulong@gmail.com>
2014-04-05 01:12:42 +0800

    Best Time to Buy and Sell Stock (rename original MaxProfit.java with MaxProfitII.java)

--- src/leetcode_java/MaxProfit.java
+++ src/leetcode_java/MaxProfit.java
@@ -1,29 +1,40 @@
 package leetcode_java;
 
 public class MaxProfit {
+	/**
+	 * 最大收益或者在以最低价买入时获得，或者在以最高价卖出时获得
+	 * 从前往后寻找以最低价买入时能获得的最大受益profit1，
+	 * 同时从后往前寻找以最高价卖出时的最大收益profit2，
+	 * 二者中的较大值即为可以获取到的最大收益。
+	 * @param prices
+	 * @return
+	 */
 	public static int maxProfit(int[] prices) {
-		int len = prices.length;
-		if (len <= 1) {
+		if (prices == null || prices.length <= 1) {
 			return 0;
 		}
-		int buy, sell, profit = 0;
-		for (int i = 0, j = 1; i < len && j < len;) {
-			buy = prices[i];
-			sell = prices[j];
-			if (buy >= sell) {
-				i = j;
-				j++;
-			} else {
-				profit += sell - buy;
-				i = j;
-				j++;
+		int min, max, profit1 = 0, profit2 = 0, len = prices.length;
+		min = prices[0];
+		max = prices[len - 1];
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] - min > profit1) {
+				profit1 = prices[i] - min;
+			}
+			if (prices[i] < min) {
+				min = prices[i];
+			}
+			if (max - prices[len - i - 1] > profit2) {
+				profit2 = max - prices[len - i - 1];
+			}
+			if (prices[len - i - 1] > max) {
+				max = prices[len - i - 1];
 			}
 		}
-
-		return profit;
+		int ans = Math.max(profit2, profit1);
+		return ans < 0 ? 0 : ans;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(maxProfit(new int[] { 2, 1, 5, 10}));
+		System.out.println(maxProfit(new int[] { 2, 3, 1, 5 }));
 	}
 }
---
+++ src/leetcode_java/MaxProfitII.java
@@ -0,0 +1,29 @@
+package leetcode_java;
+
+public class MaxProfitII {
+	public static int maxProfit(int[] prices) {
+		int len = prices.length;
+		if (len <= 1) {
+			return 0;
+		}
+		int buy, sell, profit = 0;
+		for (int i = 0, j = 1; i < len && j < len;) {
+			buy = prices[i];
+			sell = prices[j];
+			if (buy >= sell) {
+				i = j;
+				j++;
+			} else {
+				profit += sell - buy;
+				i = j;
+				j++;
+			}
+		}
+
+		return profit;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[] { 2, 1, 5, 10}));
+	}
+}
